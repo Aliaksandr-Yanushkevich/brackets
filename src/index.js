2b449bce4c40b8e0dae9d2861e0c47c394026cfd
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2019-10-10 00:51:09 +0300

    feat: solve 13/20

--- src/index.js
+++ src/index.js
@@ -10,11 +10,15 @@ module.exports = function check(str, bracketsConfig) {
           }
         }
         for (let i = 0; i < input.length; i++) {
-          if (config.indexOf(input[i])%2 !== 0 && input[i -1] === config[config.indexOf(input[i - 1])]) { // searching close bracket. close bracket index in config is odd.
+          let configIndex = config.indexOf(input[i]); // input bracket position in config arr
+          let prevConfigIndex = configIndex - 1 // previous input bracket position in config arr
+          if (configIndex%2 == 1 && input[i - 1] === config[prevConfigIndex]) { // searching close bracket. close bracket index in config is odd.
             //previous input arr element should be equal previous element in config arr
             input.splice(i - 1, 2);
             i = 0;
-          } 
+          } else {
+            continue;
+          }
         }
         
         if (input.length !== 0) { // checking input array. if it is empty (open and close brackets should kill each other) return 'true', otherwise 'false'
