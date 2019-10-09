829bafd049c5bdb142729142c0bda5b6c96215a5
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2019-10-09 23:40:15 +0300

    feat: solve 12/20

--- src/index.js
+++ src/index.js
@@ -1,32 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
       let input = str.split('');
-      let config = []
+      let config = [];
+      let answer = true;
 
         for (let i = 0; i < bracketsConfig.length; i++) {
           for (let j = 0; j < bracketsConfig[i].length; j++) {
             config.push(bracketsConfig[i][j]); // transform bracketsConfig into 1-demension array
           }
         }
-        let summ = 0;
-        let bracketSet = [];
         for (let i = 0; i < input.length; i++) {
-          let configIndex = config.indexOf(input[i]);
-          if (summ >= 0 && configIndex % 2 == 0) {
-            summ ++;
-            bracketSet.push(input[i]);
-            
-          } else if (summ >= 0 && configIndex % 2 == 1) {
-              if (input[i-1] == bracketSet.pop()) {
-                summ --;
-              } else {
-                input = false;
-              }
-          }
+          if (config.indexOf(input[i])%2 !== 0 && input[i -1] === config[config.indexOf(input[i - 1])]) { // searching close bracket. close bracket index in config is odd.
+            //previous input arr element should be equal previous element in config arr
+            input.splice(i - 1, 2);
+            i = 0;
+          } 
         }
-        if (summ !== 0) {
-          input = false;
+        
+        if (input.length !== 0) { // checking input array. if it is empty (open and close brackets should kill each other) return 'true', otherwise 'false'
+          answer = false;
         }
-      return input
+        
+      
+      return answer;
     }
-    // console.log(check(']]]{{{}}}', [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']]))
