66aeddb7f7477de319a3a6bdb0b6fc60bcab1164
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2019-10-10 11:38:05 +0300

    feat: solve 20/20

--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-      let input = str.split('');
-      let config = [];
-      let answer = true;
+  let input = str.split('');
+  let answer;
+  let configCloseBracket;
+  let configOpenBracket;
 
-        for (let i = 0; i < bracketsConfig.length; i++) {
-          for (let j = 0; j < bracketsConfig[i].length; j++) {
-            config.push(bracketsConfig[i][j]); // transform bracketsConfig into 1-demension array
-          }
+      for (let i = 0; i < input.length; i++) {
+       bracketsConfig.forEach(element => {
+        if (input[i] === element[1]) { //searching for close bracket in config
+          // console.log('i: ' + i); 
+          configCloseBracket = element[1];
+          // console.log('configCloseBracket: ' + configCloseBracket + ' input[i]: ' + input[i]);
+          configOpenBracket = element[0];
+          // console.log('configOpenBracket: ' + configOpenBracket + ' input[i - 1] : ' + input[i - 1]);
         }
-        for (let i = 0; i < input.length; i++) {
-          let configIndex = config.indexOf(input[i]); // input bracket position in config arr
-          let prevConfigIndex = configIndex - 1 // previous input bracket position in config arr
-          if (configIndex%2 == 1 && input[i - 1] === config[prevConfigIndex]) { // searching close bracket. close bracket index in config is odd.
-            //previous input arr element should be equal previous element in config arr
-            input.splice(i - 1, 2);
-            i = 0;
-          } else {
-            continue;
-          }
-        }
-        
-        if (input.length !== 0) { // checking input array. if it is empty (open and close brackets should kill each other) return 'true', otherwise 'false'
-          answer = false;
-        }
-        
-      
+      });
+      if (input[i] ==  configCloseBracket && input[i - 1] == configOpenBracket) {
+        // console.log('input arr before splice: ' + input);
+        input.splice(i-1, 2);
+        // console.log('input arr after splice: ' + input);
+        // console.log('input.length arr after splice: ' + input.length);
+        i = 0;
+      }
+      }
+      if (input.length !== 0) {
+        answer = false;
+      } else {
+        answer = true;
+      }
+      // console.log('input: ' + input);
+      // console.log('answer: ' + answer);
       return answer;
-    }
+    }   
+
